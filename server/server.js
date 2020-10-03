5c8f624260773f5ae972d97ff9a053865dd329e1
Elliot Cheung <elliotccheung@gmail.com>
2020-10-02 20:00:02 -0500

    Added function to parse csv into objects on server and endpoints toaccess the object

--- server/server.js
+++ server/server.js
@@ -1,7 +1,67 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var browserify = require('browserify-middleware');
+let express = require('express');
+let path = require('path');
+let app = express();
+let browserify = require('browserify-middleware');
+let fetch = require('node-fetch');
+
+/*
+* Function to parse the index CSV into a useful object
+* Assumes the CSV is formatted as a table with \n separating rows, and commas separating columns
+* Return object formatted as: {Country: {key: value}}
+*/
+let parseIndexCSV = function(csv) {
+	if(!csv) return {};
+
+	let arr = csv.split('\r\n');
+	if(arr.length <= 0) return {};
+
+	let index = {};
+	let keys = ['country','date','localPrice','dollarEx','dollarPrice','ppp','valuation'];
+
+	for(let i = 1; i < arr.length; i++) {
+		if(arr[i].length <= 0) continue;
+
+		let row = arr[i].split(',');
+		let country = row[0];
+		let date = Date.parse(row[1]);
+		if(index[country] != undefined && index[country].date >= date) {
+			// if we already have parsed a row, and it's more recent than the current value, quit out
+			continue;
+		}
+		if(index[country] == undefined) {
+			index[country] = {'date':date} //initialize new country
+		}
+
+		for(let j = 2; j < row.length; j++) {
+			index[country][keys[j]] = row[j]; //save values into index
+		}
+	}
+	return index;
+}
+
+let port = process.env.PORT || 4000;
+
+let startListening = function() {
+	let listener = app.listen(port);
+	console.log("Listening on localhost:" + port);
+
+	process.on('SIGINT', function(){
+		console.log("Test!")
+	  	listener.close();
+		process.exit(0);
+	});
+}
+
+let countryIndex = {};
+
+//Fetch raw table, cache the values, then start listening
+fetch("https://raw.githubusercontent.com/zelima/big-mac-index/master/data/big-mac-index.csv")
+.then((res) => (res.text()))
+.then((body) => (countryIndex = parseIndexCSV(body)))
+.then(() => (startListening()))
+.catch((err) => (console.log(err)))
+
+
 app.use(express.static(path.join(__dirname, "../client/public")));
 
 app.get('/bundle.js',
@@ -10,6 +70,8 @@ app.get('/bundle.js',
   })
 );
 
-var port = process.env.PORT || 4000;
-app.listen(port);
-console.log("Listening on localhost:" + port);
+app.get('/countries',
+	(req, res) => {
+		res.send(countryIndex);
+		res.status(200).send();
+	})
